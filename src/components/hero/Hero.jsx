d845fe3db4709ea41cf9cb0e1d682ff726f48c9c
Akbar Iskanderov <iskanderovofficial@gmail.com>
2024-06-27 08:02:10 +0500

    finished

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,7 @@ import heroImg from "../../images/hero.png";
 
 const Hero = () => {
   return (
-    <div className="container max-w-[1200px] mx-auto px-3 gap-2 grid grid-cols-1 lg:gird-cols-2 md:grid-cols-1 sm:grid-cols-1 items-center lg:mb-60 md:mb-30 sm:mb-16 mb-10">
+    <div className="container max-w-[1200px] mx-auto px-3 gap-2 grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 items-center lg:mb-60 md:mb-30 sm:mb-16 mb-10">
       <div>
         <strong className="text-[#F4F4F4] font-normal text-xs uppercase tracking-[3px]">WEB 3 NON-FUNGIBLE TOKENS</strong>
         <h1 className="font-nunito text-[60px] font-medium leading-[84px] tracking-[-0.3%] ">Unlock Unique Digital Ownership with NFTs</h1>
